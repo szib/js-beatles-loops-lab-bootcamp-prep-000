40edec9a1df048b96e453734f78f435ac140dae3
Ivan Szebenszki <szib77@gmail.com>
2019-02-27 23:07:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ function johnLennonFacts(facts) {
   }
   return arr
 }
+
+function iLoveTheBeatles(num) {
+  var arr = []
+  var i = 0;
+  do {
+    arr.push('I love the Beatles!')
+    i++
+  } while (i < num)
+  return arr
+}
