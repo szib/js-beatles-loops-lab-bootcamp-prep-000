add16683a8f3824163bbb90c645b1424af253cf6
Ivan Szebenszki <szib77@gmail.com>
2019-02-27 23:01:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var arr = []
+  for(var i = 0, i<musicians.length; i++) {
+    var s = `${musicians[i]} plays ${instruments[i]}`;
+    arr = [...arr, s]
+  }
+  return arr
+}
